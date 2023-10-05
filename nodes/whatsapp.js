0b8a63dbc11cb92316ef589fb46b482a08d09ed6
Daniel Lippert <lippertd@MacBook.fritz.box>
2023-10-05 20:35:47 +0200

    Using variables for the Credentials

--- nodes/whatsapp.js
+++ nodes/whatsapp.js
@@ -34,12 +34,51 @@ module.exports = function(RED) {
 			if(config.inputtypemessage === "msg"){
 				var buildText = eval("msg." + config.text)
 				SendText = buildText;
+            }else if(config.inputtypemessage === "flow"){
+                SendText = this.context().flow.get(config.text);
+			}else if(config.inputtypemessage === "global"){
+                SendText = this.context().global.get(config.text);
 			}else{
 				SendText = config.text;
 			}
             
             //Build Request-Link
-            var reqUrl = RootLink + "phone=" + this.credentials.phonenumber +"&text=" + encodeURIComponent(SendText)+ "&apikey=" + this.credentials.key + "&source=nodered";
+            var reqUrl = "";
+            var phonenr = "";
+            var key = "";
+
+            if(config.credtype === "account"){
+                phonenr = this.credentials.phonenumber;
+                key = this.credentials.key;
+
+                //reqUrl = RootLink + "phone=" + this.credentials.phonenumber +"&text=" + encodeURIComponent(SendText)+ "&apikey=" + this.credentials.key + "&source=nodered";
+                //node.status({ fill: "red", shape: "dot", text: reqUrl });
+            }else{
+                reqUrl = RootLink + "phone=";
+                if(config.phonenumberinputtypemessage === "msg"){
+                    phonenr = eval("msg." + config.phonenumbervalue);
+                }else if(config.phonenumberinputtypemessage === "flow"){
+                    phonenr = this.context().flow.get(config.phonenumbervalue);
+                }else if(config.phonenumberinputtypemessage === "global"){
+                    phonenr = this.context().global.get(config.phonenumbervalue);
+                }
+
+                reqUrl += "&text=" + encodeURIComponent(SendText) + "&apikey=";
+
+                if(config.apikeyinputtypemessage === "msg"){
+                    key = eval("msg." + config.apikeyvalue);
+                    
+                }else if(config.apikeyinputtypemessage === "flow"){
+                    key = this.context().flow.get(config.apikeyvalue);
+                }else if(config.apikeyinputtypemessage === "global"){
+                    key = this.context().global.get(config.apikeyvalue);
+                }
+            }
+
+            reqUrl = RootLink + "phone=" + phonenr +"&text=" + encodeURIComponent(SendText)+ "&apikey=" + key + "&source=nodered";
+            msg.requestUrl = reqUrl;
+            msg.phoneNumber = phonenr;
+
             //Run Request
             var request;
             if(config.rejectssl){
@@ -54,13 +93,15 @@ module.exports = function(RED) {
                     } else {
                         if (body.includes('APIKey is invalid')){
                             node.error("API-KEY not valid! Please check your phone number or API-KEY!")
+                            node.status({ fill: "red", shape: "dot", text: "Failed" });
                         }else{
                             msg.payload = "Message '" + SendText + "' was send successfully";
                             node.send(msg);
+                            node.status({ fill: "green", shape: "dot", text: "Message was send to " + phonenr });
                         }
                     }
                 } catch(error) {
-                        node.error(error)
+                    node.error(error)
                 }
             })
         })
